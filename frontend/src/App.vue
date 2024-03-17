b60bde56ca1d47a89af80744b8d054e8fafb299c
monse-arriaga <monse_arriaga@ciencias.unam.mx>
2024-03-17 06:02:06 -0600

    Ya hay logos, los botones funcionan y se agregaron nuevas paginas para navegar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,50 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!--npm install vue-router @types/vue-router-->
+
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<PaginaInicio msg="Monse es cool xD" />
+<div class = "container">
+  <div class="corner-buttons">
+    <div @click="() => goToPage('/')"  class="corner-button">
+      <img src="./assets/ImagenesMenuPrincipal/Home.png"alt="Botón 1" class="button-image" />
+    </div>
+
+    <!-- Espacio entre botones -->
+    <div style="margin-bottom: 10px;"></div> 
+
+    <div @click="() => goToPage('./BusquedaTorneos')"  class="corner-button">
+      <img src="./assets/ImagenesMenuPrincipal/Trofeo.png" alt="Botón 2" class="button-image" />
+    </div>
+
+    <!-- Espacio entre botones -->
+    <div style="margin-bottom: 10px;"></div> 
+
+    <div @click="() => goToPage('./BusquedaTorneos')"  class="corner-button">
+      <img src="./assets/ImagenesMenuPrincipal/configuracion.png" alt="Botón 3" class="button-image" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+    <router-view /> <!-- Esto es importante para que vue-router renderice los componentes correspondientes -->
+
+        <!-- Contenido estático de la página de inicio -->
+    <div class="pagina-inicio">
+      <PaginaInicio msg="Monse es cool xD" />
+    </div>
+  </div>
+
+
 </template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goToPage = (route: string) => {
+  router.push({ path: route });
+};
+</script>
+
 
 <style scoped>
 .logo {
@@ -26,5 +58,29 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Centra horizontalmente */
+}
+
+.corner-buttons {
+  position: fixed; /* Posicionamiento fijo */
+  top: 180px; /* Distancia desde la parte superior */
+  left: 20px; /* Distancia desde la derecha */
+}
+
+.corner-button {
+  cursor: pointer; /* Cambia el cursor al pasar sobre los botones */
+}
+
+.button-image {
+  width: 40px; /* Ajusta el ancho de la imagen */
+  height: auto; /* Permite que la altura se ajuste automáticamente para mantener la proporción */
 }
 </style>
---
+++ frontend/src/components/PaginaInicio.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+
+
+  <p>
+    Pagina Inicio!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter 
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888888;
+}
+</style>
