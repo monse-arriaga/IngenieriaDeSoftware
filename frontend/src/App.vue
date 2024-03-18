075ff51323b37a0004537d99e7edc144aafdf9d6
monse-arriaga <monse_arriaga@ciencias.unam.mx>
2024-03-17 19:10:39 -0600

    Se añadio registro, aunque falta que la info se mande al back, crear un modal de inicio de sesion y la instruccion para salir del modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,8 @@
 
 
 <template>
-<PaginaInicio msg="Monse es cool xD" />
 <div class = "container">
+
   <div class="corner-buttons">
     <div @click="() => goToPage('/')"  class="corner-button">
       <img src="./assets/ImagenesMenuPrincipal/Home.png"alt="Botón 1" class="button-image" />
@@ -24,6 +24,12 @@
     </div>
   </div>
 
+  <button @click="showLoginModal" class="login-button">
+      Regístrate
+  </button>
+
+  <Registro v-if="showModal" />
+
     <router-view /> <!-- Esto es importante para que vue-router renderice los componentes correspondientes -->
 
         <!-- Contenido estático de la página de inicio -->
@@ -37,12 +43,21 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import Registro from './components/Registro.vue';
 
 const router = useRouter();
 
 const goToPage = (route: string) => {
   router.push({ path: route });
 };
+
+const showModal = ref(false);
+
+const showLoginModal = () => {
+  showModal.value = true;
+};
+
 </script>
 
 
@@ -57,7 +72,7 @@ const goToPage = (route: string) => {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #4f34c7aa);
 }
 </style>
 
@@ -82,5 +97,21 @@ const goToPage = (route: string) => {
 .button-image {
   width: 40px; /* Ajusta el ancho de la imagen */
   height: auto; /* Permite que la altura se ajuste automáticamente para mantener la proporción */
+
 }
-</style>
+
+.login-button {
+  position: absolute; /* Posicionamiento absoluto */
+  top: 20px; /* Distancia desde la parte superior */
+  right: 20px; /* Distancia desde la derecha */
+  cursor: pointer; /* Cambia el cursor al pasar sobre los botones */
+  padding: 8px 16px; /* Añade un poco de espacio alrededor del texto */
+  border: 1px solid #2e0458; /* Agrega un borde */
+  border-radius: 4px; /* Añade bordes redondeados */
+  background-color: #27e6ba; /* Cambia el color de fondo */
+}
+
+.pagina-inicio {
+  margin-top: 60px; /* Espacio entre el menú y el contenido de la página */
+}
+</style>./components/Registro.vue
