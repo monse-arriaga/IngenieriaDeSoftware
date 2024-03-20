3be198eed4cb1d1226279c877a4600cdf6b9f341
ImBetaRay <lazaro.rodriguez@ciencias.unam.mx>
2024-03-19 22:42:18 -0600

    Se modifico el formulario de registro, se implementó el de inicio de sesión

---
+++ frontend/src/components/InicioSesion.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="modal">
+      <div class="modal-content">
+        <!-- Contenido del formulario de inicio de sesión -->
+        <button class="close-button" @click="closeForm">x</button>
+        <h2 class="title">Inicia Sesion:</h2>
+        <form @submit.prevent="handleSubmit">
+          <!-- Campo de correo -->
+          <div class="input-wrapper">
+            <input type="text" placeholder="Correo" v-model="email" required>
+          </div>
+          <!-- Campo de contraseña -->
+          <div class="input-wrapper">
+            <input type="password" placeholder="Contraseña" v-model="password" required>
+          </div>
+          <!-- Input para la fecha de nacimiento -->
+          <button type="submit">Iniciar Sesion</button>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, defineEmits } from 'vue';
+  
+  const emits = defineEmits(['closeInicio']);
+  const email = ref('');
+  const username = ref('');
+  const password = ref('');
+  const fechaNacimiento = ref('');
+  
+  const handleSubmit = () => {
+    //  Por implementar
+   };
+  
+  const closeForm = () => {
+    emits('closeInicio');
+  }
+  </script>
+  
+  <style scoped>
+  .modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(4, 1, 19, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .modal-content {
+    background-color: rgb(36, 28, 46);
+    padding: 20px;
+    border-radius: 8px;
+    position: relative;
+  }
+  
+   .title {
+    color: white; /* Cambia el color del texto a blanco */
+  }
+  
+  .input-wrapper {
+    margin-bottom: 10px; /* Espacio entre los campos de entrada */
+  }
+  
+  .separator {
+    margin: 20px 0; /* Espacio entre la fecha de nacimiento y los otros datos */
+    border: none;
+    border-top: 1px solid #ccc;
+  }
+   
+  .close-button {
+    position: absolute;
+    top: -10px;
+    right: -10px;
+    background: none;
+    border: none;
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+    z-index: 1;
+  }
+  
+  .close-button:hover {
+    color: red;
+  } 
+  /* Ajusta los estilos según sea necesario */
+  </style>
+  
--- frontend/src/components/Registro.vue
+++ frontend/src/components/Registro.vue
@@ -1,71 +1,100 @@
 <template>
-    <div class="modal">
-      <div class="modal-content">
-        <!-- Contenido del formulario de inicio de sesión -->
-        <h2 class="title">Regístrate</h2>
-        <form @submit.prevent="handleSubmit">
-          <!-- Campos de entrada de usuario y contraseña -->
+  <div class="modal">
+    <div class="modal-content">
+      <!-- Contenido del formulario de inicio de sesión -->
+      <button class="close-button" @click="closeForm">x</button>
+      <h2 class="title">Empieza tu aventura ahora:</h2>
+      <form @submit.prevent="handleSubmit">
+        <!-- Campo de correo -->
+        <div class="input-wrapper">
+          <input type="text" placeholder="Correo" v-model="email" required>
+        </div>
+        <!-- Campo de nombre de usuario -->
+        <div class="input-wrapper">
           <input type="text" placeholder="Nombre de usuario" v-model="username" required>
+        </div>
+        <!-- Campo de contraseña -->
+        <div class="input-wrapper">
           <input type="password" placeholder="Contraseña" v-model="password" required>
-          <br />
-          <br />
+        </div>
         <!-- Input para la fecha de nacimiento -->
-        <label style="color: white;"  for="fechaNacimiento">Fecha de nacimiento: </label>
-        <input type="date" id="fechaNacimiento" v-model="fechaNacimiento" required>
-            </form>
-        <br />
-        <br />
-          <button type="submit">Regístrate</button>
-          <br />
-          <p style="color: white;">¿Ya tienes una cuenta? <a href="#">Inicia sesión</a></p>
-      </div>
+        <button type="submit">Regístrate</button>
+        <p style="color: white;">¿Ya tienes una cuenta? <a href="#" @click="showLogin">Inicia sesión</a></p>
+      </form>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const username = ref('');
-  const password = ref('');
-  const fechaNacimiento = ref('');
-  
-  const handleSubmit = () => {
-    //  lógica para enviar los datos de inicio de sesión al servidor
+  </div>
+</template>
 
-    // Luego, cierra el modal
-    // Puedes emitir un evento o cambiar una propiedad para controlar la visibilidad del modal desde fuera
-  };
-  </script>
-  
-  <style scoped>
-  .modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(4, 1, 19, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .modal-content {
-    background-color: rgb(36, 28, 46);
-    padding: 20px;
-    border-radius: 8px;
-  }
+<script setup>
+import { ref, defineEmits } from 'vue';
 
-   .title {
-    color: white; /* Cambia el color del texto a blanco */
-  }
+const emits = defineEmits(['closeRegistro', 'showLogInInicio']);
+const email = ref('');
+const username = ref('');
+const password = ref('');
+const fechaNacimiento = ref('');
 
-  .separator {
+const handleSubmit = () => {
+  //Por implementar
+};
+
+const closeForm = () => {
+  emits('closeRegistro');
+}
+
+const showLogin = () => {
+  emits('showLogInInicio');
+}
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(4, 1, 19, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: rgb(36, 28, 46);
+  padding: 20px;
+  border-radius: 8px;
+  position: relative;
+}
+
+ .title {
+  color: white; /* Cambia el color del texto a blanco */
+}
+
+.input-wrapper {
+  margin-bottom: 10px; /* Espacio entre los campos de entrada */
+}
+
+.separator {
   margin: 20px 0; /* Espacio entre la fecha de nacimiento y los otros datos */
   border: none;
   border-top: 1px solid #ccc;
-  }
-    
-  /* Ajusta los estilos según sea necesario */
-  </style>
-  
+}
+ 
+.close-button {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  z-index: 1;
+}
+
+.close-button:hover {
+  color: red;
+} 
+/* Ajusta los estilos según sea necesario */
+</style>
