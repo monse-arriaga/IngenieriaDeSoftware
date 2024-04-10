d42327eeb06d90192de19c041abe3ebde0351ffb
Julian Rosas <julianrosas11032002@ciencias.unam.mx>
2024-04-09 21:49:38 -0500

    JWT implementado

---
+++ backend/src/main/java/unam/ciencias/ids/playbit/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package unam.ciencias.ids.playbit.payload.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- backend/src/main/java/unam/ciencias/ids/playbit/services/UserServices.java
+++ backend/src/main/java/unam/ciencias/ids/playbit/services/UserServices.java
@@ -38,4 +38,5 @@ public class UserServices {
 
         return false;
     }
+
 }
