713f8236777e7b7fdfd88e09000c9412ce99b83a
monse-arriaga <monse_arriaga@ciencias.unam.mx>
2024-05-27 10:11:31 -0600

    Ya (supuestamente) estan juntados el front y back

--- frontend/src/components/Torneo.vue
+++ frontend/src/components/Torneo.vue
@@ -1,49 +1,64 @@
 <template>
   <div class="tournament-info">
-  <div v-if="loading">
-    <h1>Loading...</h1>
-  </div>
-  <div v-else-if="tournamentDetails">
-    <q-img class="col-2"
-    :src="tournamentDetails.image"
-     style="width: 1250px; height: 700px; object-fit: cover;"/>
-
-  <div class="tournament-info">
-    <h6 class="text-grey-5 q-gutter-md; " >
-      <q-icon name="lock_open" />{{ '   '+tournamentDetails.state}}  
-      <q-icon name="sports_esports" />{{ '   '+tournamentDetails.tournamentGame}}  
-      <q-icon name="sports_kabaddi" />{{ '   '+tournamentDetails.tournamentType}}</h6>
-    <h1 class="text-white q-gutter-md; " >{{ tournamentDetails.name }}</h1>
-    <h4 class="text-grey-5 q-gutter-md; " >{{ tournamentDetails.description }}</h4>
-    <div class="tournament-info">
-    <h6 class="text-grey-5 q-gutter-md; " >
-      <q-icon name="today" />{{ '   '+tournamentDetails.date}}  
-      <q-icon name="schedule" />{{ '   '+tournamentDetails.time}}  
-      <q-icon name="groups" />{{ '   '+tournamentDetails.inPlayers+'/'+tournamentDetails.players}}</h6>
+    <div v-if="loading">
+      <h1>Loading...</h1>
     </div>
-  </div >
-      <!-- Render other tournament details here -->
-  </div>
-  <div v-else>
-    <h1>No carga lol</h1>
-  </div>
-</div>
 
-<div class="tournament-info">
-  <div v-if="isLoggedIn">
-    <q-btn v-if="!isEnrolled" id="plus-button" color="primary" @click="enroll">
-      <strong>Inscribete</strong> 
-    </q-btn>   
-    <q-btn v-else id="plus-button" color="primary" >
-      <strong>Ya estás inscrito</strong> 
-    </q-btn>         
+    <div v-else-if="tournamentDetails">
+      <q-tabs class="text-white" active-color="bg-indigo-6" v-model="tab">
+        <q-tab name="details" label="Detalles del Torneo" />
+        <q-tab name="other" label="Clasificatorias" />
+      </q-tabs>
+
+      <q-tab-panels v-model="tab">
+        <q-tab-panel name="details">
+          <q-img class="col-2"
+            :src="tournamentDetails.image"
+            style="width: 1250px; height: 700px; object-fit: cover;"/>
+          <div class="tournament-info">
+            <h6 class="text-grey-8 q-gutter-md;">
+              <q-icon name="lock_open" />{{ '   '+tournamentDetails.state}}  
+              <q-icon name="sports_esports" />{{ '   '+tournamentDetails.tournamentGame}}  
+              <q-icon name="sports_kabaddi" />{{ '   '+tournamentDetails.tournamentType}}
+            </h6>
+            <h1 class="text-black q-gutter-md;">{{ tournamentDetails.name }}</h1>
+            <h4 class="text-grey-8 q-gutter-md;">{{ tournamentDetails.description }}</h4>
+            <div class="tournament-info">
+              <h6 class="text-grey-5 q-gutter-md;">
+                <q-icon name="today" />{{ '   '+tournamentDetails.date}}  
+                <q-icon name="schedule" />{{ '   '+tournamentDetails.time}}  
+                <q-icon name="groups" />{{ '   '+tournamentDetails.inPlayers+'/'+tournamentDetails.players}}
+              </h6>
+            </div>
+          </div>
+        </q-tab-panel>
+
+        <q-tab-panel name="other">
+          <h1>Aquí hay otra tab</h1>
+        </q-tab-panel>
+      </q-tab-panels>
+    </div>
+
+    <div v-else>
+      <h1>No carga lol</h1>
+    </div>
+
+    <div class="tournament-info">
+      <div v-if="isLoggedIn">
+        <q-btn v-if="!isEnrolled" id="plus-button" color="primary" @click="enroll">
+          <strong>Inscribete</strong> 
+        </q-btn>   
+        <q-btn v-else id="plus-button" color="primary">
+          <strong>Ya estás inscrito</strong> 
+        </q-btn>         
+      </div>
+      <div v-else>
+        Inicia Sesión Por Favor
+      </div>
+    </div>
   </div>
-  <div v-else>
-    Inicia Sesión Por Favor
-  </div>
-</div>
 </template>
-  
+
 <script lang="ts">
 import { computed, defineComponent, onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
@@ -52,145 +67,69 @@ import authHeader from "../services/auth-header";
 import { useUserStore } from "../store/user";
 import TorneoCarta from "./TorneoCarta.vue";
 import TournamentService from "../services/TournamentService";
-<<<<<<< HEAD
-import { BracketsManager } from 'brackets-manager'
+import { BracketsManager } from 'brackets-manager';
 import { tournamentStorage } from "../store/tournament";
-=======
 import Tournament from "../types/Tournament";
->>>>>>> vistas-torneo
-
 
 export default defineComponent({
   name: "Detalles",
   components: {
     TorneoCarta
-<<<<<<< HEAD
   },
   setup() {
     const router = useRouter();
-    const tournament = router.currentRoute.value.params.tournament;
+    const tournamentName = router.currentRoute.value.params.tournament.toString();
     const userStore = useUserStore();
     const isLoggedIn = computed(() => userStore.isLoggedIn);
     const isEnrolled = ref(false);
-    const tournamentO = ref("");
-    const storage = new tournamentStorage();
-    const manager = new BracketsManager(storage);
-
-    
+    const tournamentDetails = ref<Tournament | null>(null);
+    const loading = ref(true); // Add loading state
+    const tab = ref('details'); // Add ref for the tab
 
     const checkEnrolled = async () => {
-        try {
-          const enrolledTournaments = await UserService.tournaments_enrolled(authHeader().UserId);
-          isEnrolled.value = enrolledTournaments.includes(tournament.toString());
-        } catch (error) {
-          isEnrolled.value = false;
-        }
-      };
-    
-    
+      try {
+        const enrolledTournaments = await UserService.tournaments_enrolled(authHeader().UserId);
+        isEnrolled.value = enrolledTournaments.includes(tournamentName);
+      } catch (error) {
+        console.error("Error checking enrollment:", error);
+      }
+    };
 
-    const getToournament = async () => {
-        tournamentO.value = await TournamentService.getTournamentByName(tournament.toString());
-    }
-
-    async function render() {
-      const data = await manager.get.tournamentData("3");
-      console.log(data)
-      window.bracketsViewer.render({
-        stages: data.stage,
-        matches: data.match,
-        matchGames: data.match_game,
-        participants: data.participant,
-      });
-    }
+    const fetchTournamentDetails = async () => {
+      try {
+        const tournament = await TournamentService.getTournamentByName(tournamentName);
+        tournamentDetails.value = tournament[0];
+      } catch (error) {
+        console.error("Error fetching tournament details:", error);
+      } finally {
+        loading.value = false; // Set loading to false regardless of success or failure
+      }
+    };
 
     onMounted(async () => {
-        await getToournament();
-        await manager.create.stage({
-          tournamentId: 3,
-          name: 'Elimination stage',
-          type: 'double_elimination',
-          seeding: ['Team 1', 'Team 2', 'Team 3', 'Team 4'],
-          settings: { grandFinal: 'double' },
-        });
-        await render()
-      });
-        
+      await checkEnrolled();
+      await fetchTournamentDetails();
+    });
 
     const enroll = async () => {
-        try {
-          await UserService.enroll(authHeader().UserName, tournament.toString());
-          window.location.reload();
-        } catch (error) {
-          console.error("Error enrolling in tournament:", error);
-        }
-      };
-
-    
+      try {
+        await UserService.enroll(authHeader().UserName, tournamentName);
+        window.location.reload();
+      } catch (error) {
+        console.error("Error enrolling in tournament:", error);
+      }
+    };
 
     return {
       isEnrolled,
       checkEnrolled,
-      tournamentO,
       isLoggedIn,
       enroll,
-      render
+      tournamentDetails,
+      loading,
+      tab, // Return the tab ref
     };
-=======
->>>>>>> vistas-torneo
   },
-  setup() {
-  const router = useRouter();
-  const tournamentName = router.currentRoute.value.params.tournament.toString();
-  const userStore = useUserStore();
-  const isLoggedIn = computed(() => userStore.isLoggedIn);
-  const isEnrolled = ref(false);
-  const tournamentDetails = ref<Tournament | null>(null);
-  const loading = ref(true); // Add loading state
-
-  const checkEnrolled = async () => {
-    try {
-      const enrolledTournaments = await UserService.tournaments_enrolled(authHeader().UserId);
-      isEnrolled.value = enrolledTournaments.includes(tournamentName);
-    } catch (error) {
-      console.error("Error checking enrollment:", error);
-    }
-  };
-
-  const fetchTournamentDetails = async () => {
-    try {
-      const tournament = await TournamentService.getTournamentByName(tournamentName);
-      tournamentDetails.value = tournament[0];
-    } catch (error) {
-      console.error("Error fetching tournament details:", error);
-    }finally {
-        loading.value = false; // Set loading to false regardless of success or failure
-      }
-  };
-
-  onMounted(async () => {
-    await checkEnrolled();
-    await fetchTournamentDetails();
-  });
-
-  const enroll = async () => {
-    try {
-      await UserService.enroll(authHeader().UserName, tournamentName);
-      window.location.reload();
-    } catch (error) {
-      console.error("Error enrolling in tournament:", error);
-    }
-  };
-
-  return {
-    isEnrolled,
-    checkEnrolled,
-    isLoggedIn,
-    enroll,
-    tournamentDetails,
-    loading,
-  };
-},
 });
 </script>
 
@@ -198,5 +137,4 @@ export default defineComponent({
 .tournament-info {
   margin-top: 55px; /* Adjust the margin-top as needed */
 }
-
 </style>
